6b407be4d0759e06ccef981f40b57b2965b9add4
Oleg <oxegay@gmail.com>
2024-08-26 16:57:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+const body = document.querySelector('body')
+const box = document.querySelector('.box')
+const item1 = document.querySelector('h1')
+const image = document.querySelector('img')
+
+const name = document.querySelector('.box h1 span')
+const surename = document.querySelector('.box h2 span')
+
+
+// const qu1 = prompt('Укажите цвет фона')
+// const qu2 = prompt('Укажите цвет фона бокса')
+const qu3 = +prompt('Укажите ширину бокса')
+const qu4 = prompt('Укажите ссылку на картинку')
+const qu5 = prompt('Укажите ваше имя')
+const qu6 = prompt('Укажите вашу фамилию')
+
+
+// body.style.backgroundColor = qu1
+// box.style.backgroundColor = qu2
+box.style.width = qu3 + 'px'
+image.src = qu4
+name.innerText = qu5
+surename.innerText = qu6
